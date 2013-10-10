3a898f84f186e6ce9c7184e712d49b20a9ffa7aa
Axel Billingsley <axel@axelbcreative.co.uk>
2013-10-10 12:06:18 +0100

    Front-end progress so far
    All original files used to build basic front-end template

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Foundation 4</title>
+
+  
+  <link rel="stylesheet" href="css/foundation.css">
+  
+  <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+  <script src="js/vendor/custom.modernizr.js"></script>
+  <script src="js/naveffects.js"></script>
+
+</head>
+<body>
+	<div class="row">
+		<div class="large-12 large-centered columns header">
+    		<a href="index.php" title="Home"><img src="img/axelbcreative.png" alt="AxelBCreative logo" /></a>
+        </div>
+    </div>
+    
+    <hr>
+   
+    
+    <div class="row">
+    	<div class="large-3 columns nav">
+        	<a id="nav1" href="#">Home</a>
+        </div>
+        <div class="large-3 columns nav">
+        	<a id="nav2" href="#">Portfolio</a>
+        </div>
+        <div class="large-3 columns nav">
+        	<a id="nav3" href="#">Uni Work</a>
+        </div>
+        <div class="large-3 columns nav">
+        	<a id="nav4" href="#">Contact</a>
+        </div>
+    </div>
+    
+    <div class="row">
+    	<div class="large-3 columns nav">
+        	<img id="arrow1" src="img/arrow-01.png" alt="arrow" />
+        </div>
+        <div class="large-3 columns nav">
+        	<img id="arrow2" src="img/arrow-01.png" alt="arrow" />
+        </div>
+        <div class="large-3 columns nav">
+        	<img id="arrow3" src="img/arrow-01.png" alt="arrow" />
+        </div>
+        <div class="large-3 columns nav">
+        	<img id="arrow4" src="img/arrow-01.png" alt="arrow" />
+        </div>
+    </div>
+    
+    <div class="row"
+    	<div class="large-12 large-centered columns" id="content">
+        <h1>This is a heading</h1>
+        <p>This is a paragraph</p>
+        </div>
+    </div>
+        
+     
+  <script>
+  document.write('<script src=' +
+  ('__proto__' in {} ? 'js/vendor/zepto' : 'js/vendor/jquery') +
+  '.js><\/script>')
+  </script>
+  
+  <script src="js/foundation.min.js"></script>
+  <!--
+  
+  <script src="js/foundation/foundation.js"></script>
+  
+  <script src="js/foundation/foundation.interchange.js"></script>
+  
+  <script src="js/foundation/foundation.abide.js"></script>
+  
+  <script src="js/foundation/foundation.dropdown.js"></script>
+  
+  <script src="js/foundation/foundation.placeholder.js"></script>
+  
+  <script src="js/foundation/foundation.forms.js"></script>
+  
+  <script src="js/foundation/foundation.alerts.js"></script>
+  
+  <script src="js/foundation/foundation.magellan.js"></script>
+  
+  <script src="js/foundation/foundation.reveal.js"></script>
+  
+  <script src="js/foundation/foundation.tooltips.js"></script>
+  
+  <script src="js/foundation/foundation.clearing.js"></script>
+  
+  <script src="js/foundation/foundation.cookie.js"></script>
+  
+  <script src="js/foundation/foundation.joyride.js"></script>
+  
+  <script src="js/foundation/foundation.orbit.js"></script>
+  
+  <script src="js/foundation/foundation.section.js"></script>
+  
+  <script src="js/foundation/foundation.topbar.js"></script>
+  
+  -->
+  
+  <script>
+    $(document).foundation();
+  </script>
+</body>
+</html>
---
+++ js/naveffects.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$("#nav1").mouseenter(function(){
+		$("#arrow1").css("visibility","visible");
+	});
+	$("#nav1").mouseleave(function(){
+		$("#arrow1").css("visibility","hidden");
+	});
+	$("#nav2").mouseenter(function(){
+		$("#arrow2").css("visibility","visible");
+	});
+	$("#nav2").mouseleave(function(){
+		$("#arrow2").css("visibility","hidden");
+	});
+	$("#nav3").mouseenter(function(){
+		$("#arrow3").css("visibility","visible");
+	});
+	$("#nav3").mouseleave(function(){
+		$("#arrow3").css("visibility","hidden");
+	});
+	$("#nav4").mouseenter(function(){
+		$("#arrow4").css("visibility","visible");
+	});
+	$("#nav4").mouseleave(function(){
+		$("#arrow4").css("visibility","hidden");
+	});
+});
